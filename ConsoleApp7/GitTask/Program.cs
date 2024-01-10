a119254f67f35b5bc2af0314105d9a1daa072c2c
daniilPromskiy <daniil.promskiy2006@gmail.com>
2024-01-10 19:00:07 +0300

    new version

--- ConsoleApp7/GitTask/Program.cs
+++ ConsoleApp7/GitTask/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Привет");
+        
+        Console.ReadLine();
+    }
+}
